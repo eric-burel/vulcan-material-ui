35e5e08e911405fb001bcc17f4f452bef46552ab
Eric Burel <e.burel.contact@gmail.com>
2018-08-29 17:11:34 +0200

    Update package.js
    Load more recent versions of Vulcan

--- package.js
+++ package.js
@@ -12,9 +12,9 @@ Package.onUse(function (api) {
 
   api.use([
     'ecmascript',
-    'vulcan:accounts@1.3.2',
-    'vulcan:forms@1.3.2',
-    'vulcan:core@1.3.2',
+    'vulcan:accounts@1.11.2',
+    'vulcan:forms@1.11.2',
+    'vulcan:core@1.11.2',
   ]);
 
   api.addFiles('accounts.css', ['client', 'server']);
